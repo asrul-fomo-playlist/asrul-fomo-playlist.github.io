2f42fb4780875aee88dfe67deff24a5377b7edde
asrul-fomo-playlist <asrultzy21@gmail.com>
2025-05-20 10:49:58 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+    {
+        name: "Somebody's Pleasure",
+        artist: "Aziz Hedra",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "DECEMBER",
+        artist: "NECKDEEP",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Nina",
+        artist: ".Feast",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Happiness",
+        artist: "Rex Orange County",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Payphone",
+        artist: "Maroon 5",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Night Changes",
+        artist: "One Direction",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Right Now",
+        artist: "One Direction",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Strong",
+        artist: "One Direction",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Surat Cinta Untuk Starla",
+        artist: "Virgoun",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Where We Are",
+        artist: "One Direction",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Wish You Where Here",
+        artist: "NECKDEEP",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+    name: "blue",
+        artist: "Yung kai",
+        img: "music-12",
+        src: "music-12"
+    },
+];
